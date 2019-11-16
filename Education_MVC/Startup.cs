9f1873c0e1556247593de2ee6a9b02e6130cdfb9
Nguyen Le Nam Anh <48341290+nguyenlenamanh@users.noreply.github.com>
2019-11-17 00:52:51 +0700

    first commit

---
+++ Education_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Education_MVC.Startup))]
+namespace Education_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
